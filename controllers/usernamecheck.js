4e5b022c426909309ff4a0694c9b14e5336283f0
Dweep Panchal <dweep.programmer@gmail.com>
2020-05-31 16:48:20 +0530

    Add: social media checking

--- controllers/usernamecheck.js
+++ controllers/usernamecheck.js
@@ -1,5 +1,9 @@
 const fetch = require("node-fetch");
 
+/*****************************
+ * REPO HOSTING SERVICES
+ ****************************/
+
 exports.githubCheck = (req, res) => {
   var githubRegex = /^[a-zA-Z0-9]\w{0,38}/g;
 
@@ -9,6 +13,120 @@ exports.githubCheck = (req, res) => {
   ) {
     fetch("https://github.com/" + req.params.username)
       .then((response) => {
+        if (response.status === 404) {
+          return res.json({ success: "Username Available" });
+        } else {
+          return res.json({ error: "Username Taken" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    return res.status(406).json({ error: "Invalid Username" });
+  }
+};
+
+exports.gitlabCheck = (req, res) => {
+  var gitlabRegex = /^[a-zA-Z0-9]\w{0,254}/g;
+
+  if (
+    gitlabRegex.test(req.params.username) &&
+    req.params.username.length <= 255
+  ) {
+    fetch("https://gitlab.com/" + req.params.username)
+      .then((response) => {
+        if (response.url === "https://gitlab.com/users/sign_in") {
+          return res.json({ success: "Username Available" });
+        } else {
+          return res.json({ error: "Username Taken" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    return res.status(406).json({ error: "Invalid Username" });
+  }
+};
+
+exports.bitbucketCheck = (req, res) => {
+  var bitbucketRegex = /^[a-zA-Z0-9]\w{0,38}/g;
+
+  if (
+    bitbucketRegex.test(req.params.username) &&
+    req.params.username.length <= 39
+  ) {
+    fetch("https://bitbucket.org/" + req.params.username)
+      .then((response) => {
+        if (response.status === 404) {
+          return res.json({ success: "Username Available" });
+        } else {
+          return res.json({ error: "Username Taken" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    return res.status(406).json({ error: "Invalid Username" });
+  }
+};
+
+/*****************************
+ * SOCIAL MEDIA
+ ****************************/
+
+exports.facebookCheck = (req, res) => {
+  var facebookRegex = /^[a-zA-Z0-9.]\w{5,}/g;
+
+  if (facebookRegex.test(req.params.username)) {
+    fetch("https://facebook.com/" + req.params.username)
+      .then((response) => {
+        if (response.status === 404) {
+          return res.json({ success: "Username Available" });
+        } else {
+          return res.json({ error: "Username Taken" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    return res.status(406).json({ error: "Invalid Username" });
+  }
+};
+
+exports.instagramCheck = (req, res) => {
+  var instagramRegex = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{1,29}$/gim;
+
+  if (instagramRegex.test(req.params.username)) {
+    fetch("https://instagram.com/" + req.params.username)
+      .then((response) => {
+        if (response.status === 404) {
+          return res.json({ success: "Username Available" });
+        } else {
+          return res.json({ error: "Username Taken" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    return res.status(406).json({ error: "Invalid Username" });
+  }
+};
+
+exports.twitterCheck = (req, res) => {
+  var twitterRegex = /^[a-zA-Z0-9_]\w{0,14}/g;
+
+  if (
+    twitterRegex.test(req.params.username) &&
+    req.params.username.length <= 15
+  ) {
+    fetch("https://twitter.com/" + req.params.username)
+      .then((response) => {
+        console.log(response);
         if (response.status === 404) {
           return res.json({ success: "Username Available" });
         } else {
--- routes/usernamecheck.js
+++ routes/usernamecheck.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-const { githubCheck } = require("../controllers/usernamecheck");
+const {
+  githubCheck,
+  gitlabCheck,
+  bitbucketCheck,
+  facebookCheck,
+  instagramCheck,
+  twitterCheck,
+} = require("../controllers/usernamecheck");
 
+// Repo Hosting Services
 router.get("/github/:username", githubCheck);
+router.get("/gitlab/:username", gitlabCheck);
+router.get("/bitbucket/:username", bitbucketCheck);
+
+// Social Media
+router.get("/facebook/:username", facebookCheck);
+router.get("/instagram/:username", instagramCheck);
+router.get("/twitter/:username", twitterCheck);
 
 module.exports = router;
